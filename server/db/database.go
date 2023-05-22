6c6f05b15ec3c32e84a67a61cd9ec925d308cd4e
UdayOP <uday.khairnar21@gmail.com>
2023-05-22 12:04:46 +0530

    db url change

--- server/db/database.go
+++ server/db/database.go
@@ -2,16 +2,17 @@ package db
 
 import (
 	"database/sql"
+	"infraguard-manager/helpers/configHelper"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func MySqlConnection() *sql.DB {
-	//config based
-	dbUrl := "root:Digi@2023@/infraguard_manager?parseTime=true"
+
+	//read config based db url and connect
 	log.Print("Create MySql Connection")
-	sql, err := sql.Open("mysql", dbUrl)
+	sql, err := sql.Open("mysql", configHelper.GetString("DB_URL"))
 	if err != nil {
 		log.Println("Cannot connect to database", err)
 	}
